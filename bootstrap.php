5e9b2c1fd0c8b6e1b347298118029117f89f7a75
nguyenb1910266 <124817014+nguyenb1910266@users.noreply.github.com>
2023-03-19 23:59:18 +0700

    Create bootstrap.php

---
+++ bootstrap.php
@@ -0,0 +1,15 @@
+<?php
+
+define('ROOTDIR', __DIR__ . DIRECTORY_SEPARATOR);
+
+session_start();
+
+require_once ROOTDIR . 'autoload.php';
+require_once ROOTDIR . 'src/helpers.php';
+
+$PDO = (new \CT275\Labs\PDOFactory())->create([
+    'dbhost' => 'localhost',
+    'dbname' => 'ct275',
+    'dbuser' => 'root',
+    'dbpass' => ''
+]);
