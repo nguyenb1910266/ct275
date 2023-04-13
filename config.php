d2fc95b9b2be766ae35639295a766e2fee70fd81
nguyenb1910266 <124817014+nguyenb1910266@users.noreply.github.com>
2023-04-13 11:32:23 +0700

    Add files via upload

---
+++ config.php
@@ -0,0 +1,6 @@
+<?php
+    require '../vendor/autoload.php';
+    $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+    $dotenv->load();
+    $connect = mysqli_connect($_ENV['DB_HOST'], $_ENV['DB_USERNAME'], "", $_ENV['DB_NAME']);
+?>
